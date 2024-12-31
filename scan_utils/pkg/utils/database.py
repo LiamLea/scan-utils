5b2271694b8a78b1954381764973890242548b02
liamlea <liweixiliang@gmail.com>
2024-12-31 16:27:39 +0800

    fix pg get_meta_info debug

--- scan_utils/pkg/utils/database.py
+++ scan_utils/pkg/utils/database.py
@@ -1,22 +1,23 @@
 import pymysql
 import psycopg2
 
+
 class MysqlDB:
     def __init__(self, db_info):
         self.db_info = db_info
         self.conn = self.connect()
 
     def connect(self):
-        conn = pymysql.connect(host = self.db_info["host"],
-                               port = int(self.db_info["port"]),
-                               user = self.db_info["user"],
-                               password = self.db_info["passwd"],
-                               database = self.db_info["db_name"],
-                               charset = "utf8"
+        conn = pymysql.connect(host=self.db_info["host"],
+                               port=int(self.db_info["port"]),
+                               user=self.db_info["user"],
+                               password=self.db_info["passwd"],
+                               database=self.db_info["db_name"],
+                               charset="utf8"
                                )
         return conn
 
-    def exec_sql(self, sql, fetch = "Many", num = 0):
+    def exec_sql(self, sql, fetch="Many", num=0):
         cursor = self.conn.cursor()
         cursor.execute(sql)
         if fetch == "Many":
@@ -26,13 +27,14 @@ class MysqlDB:
         else:
             return cursor.fetchmany(num)
 
+
 class PostgreDB:
     def __init__(self, db_info):
         self.db_info = db_info
         self.conn = self.connect()
 
     def connect(self):
-        conn= psycopg2.connect(
+        conn = psycopg2.connect(
             database=self.db_info["db_name"],
             user=self.db_info["db_user"],
             password=self.db_info["passwd"],
@@ -41,7 +43,7 @@ class PostgreDB:
         )
         return conn
 
-    def exec_sql(self, sql, fetch = "Many", num = 0):
+    def exec_sql(self, sql, fetch="Many", num=0):
         cursor = self.conn.cursor()
         cursor.execute(sql)
         if fetch == "Many":
@@ -52,11 +54,21 @@ class PostgreDB:
             return cursor.fetchmany(num)
 
     def get_meta_info(self):
+        old_conn = self.conn
+        self.conn = psycopg2.connect(
+            database="postgres",
+            user=self.db_info["db_user"],
+            password=self.db_info["passwd"],
+            host=self.db_info["host"],
+            port=self.db_info["port"],
+        )
         result = {
             "dbs": []
         }
 
         for i in self.exec_sql("SELECT datname FROM pg_database;"):
             result["dbs"].append(i[0])
+        self.conn.close()
+        self.conn = old_conn
 
         return result
